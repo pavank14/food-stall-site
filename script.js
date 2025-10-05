c0e02a197d3ea044c2c7b7462f0c844c03decec9
pavank14 <160216842+pavank14@users.noreply.github.com>
2025-10-06 00:46:06 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -7,3 +7,15 @@ function scrollMenu(id, direction) {
   const scrollAmount = 300 * direction;
   container.scrollBy({ left: scrollAmount, behavior: "smooth" });
 }
+
+window.addEventListener("load", function () {
+  const popup = document.getElementById("popup-overlay");
+  const closeBtn = document.getElementById("close-popup");
+
+  popup.classList.remove("hidden"); // Show popup on load
+
+  closeBtn.addEventListener("click", function () {
+    popup.classList.add("hidden"); // Hide popup on click
+  });
+});
+
